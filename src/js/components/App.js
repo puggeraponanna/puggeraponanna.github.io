57cedd820d0322ca6ed1043e0ea84668f5268c7d
Ponanna P M <pponanna@pm.me>
2020-08-30 12:00:16 +0530

    Initial Commit with basic structure and base app

---
+++ src/js/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class App extends React.Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <div>
+                <h1>Ponanna P M</h1>
+            </div>
+        );
+    }
+}
