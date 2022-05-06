11828c5bee1a28b165461d65e3f6e250136a8345
puggeraponanna <ponannapuggera@gmail.com>
2022-05-06 09:40:38 +0530

    moving to nextjs

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Link from 'next/link'
+export default function Home() {
+  return (
+    <div>
+        <h1>
+      Ponanna Puggera
+        </h1>
+    </div>
+  )
+}
